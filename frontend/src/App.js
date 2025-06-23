155ca8f92920e2e567691686af0d0a4bc8c8f241
Manideep-Ram-Gunje <manideepram2006c@gmail.com>
2025-06-23 23:56:01 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,45 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate, Link } from "react-router-dom";
 
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import AssignmentList from "./pages/AssignmentList";        // Student dashboard
-// import TutorAssignments from "./pages/TutorAssignments";    // Tutor dashboard
 import SubmitAssignmentPage from "./pages/SubmitAssignmentPage";
+import TutorAssignmentList from "./pages/TutorAssignmentList";
+import CreateAssignmentPage from "./pages/CreateAssignmentPage";
+
+import "./App.css"; // if you have styling here
 
 const App = () => {
+  // Retrieve user info from localStorage (assumes login stores it)
+  const storedUser = localStorage.getItem("user");
+  const user = storedUser ? JSON.parse(storedUser) : null;
+
   return (
     <Router>
-      <Routes>
-        {/* Redirect root "/" to "/login" */}
-        <Route path="/" element={<Navigate to="/login" />} />
+      {/* Conditional Tutor Nav */}
+      {user?.role === "tutor" && (
+        <nav className="tutor-nav">
+          <Link to="/tutor/assignments" className="nav-link">📘 My Assignments</Link>
+          <Link to="/tutor/create-assignment" className="nav-link">➕ Create Assignment</Link>
+        </nav>
+      )}
 
+      <Routes>
         {/* Public pages */}
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
 
-        {/* Dashboards */}
-        <Route path="/assignments" element={<AssignmentList />} />
-        {/* <Route path="/tutor-assignments" element={<TutorAssignments />} /> */}
+        {/* Student & Submission pages */}
+        <Route path="/assignments" element={<AssignmentList user={user} />} />
         <Route path="/assignments/:assignmentId/submit" element={<SubmitAssignmentPage />} />
 
+        {/* Tutor pages */}
+        <Route path="/tutor/assignments" element={<TutorAssignmentList user={user} />} />
+        <Route path="/tutor/create-assignment" element={<CreateAssignmentPage user={user} />} />
 
-        {/* Catch-all unmatched routes redirect to login */}
+        {/* Catch-all */}
         <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
     </Router>
